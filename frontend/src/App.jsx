b3de1130a08c1860bb6e62c231ab30837637b38b
Niladri2003 <niladri.adak.21@gmail.com>
2024-04-08 00:51:46 +0530

    git updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     // Fetch step count data from server
     const fetchStepCount = async () => {
         try {
-            const response = await axios.get('http://localhost:8000/stepsdata');
+            const response = await axios.get('https://git.heroku.com/iot.git/stepsdata');
             console.log("Response=>",response);
             setStepCount(response.data.data.steps);
             setLastUpdated(response.data.data.lastUpdatedTimestamp);
